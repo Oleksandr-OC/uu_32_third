34a406a6ed0c2200a2232e51db49a1c7c95a7210
Oleksandr-OC <271310948+Oleksandr-OC@users.noreply.github.com>
2026-05-05 18:58:26 +0200

    realisation+++ Product.cpp

--- urok_32_third/Product.cpp
+++ urok_32_third/Product.cpp
@@ -1,1 +1,75 @@
 #include "Product.h"
+
+Products::Product::Product()
+{
+    // N/D = not defined
+    name = "N/D";
+    price = 0.0;
+    manufacture = "N/D"; 
+    age = 0;
+    quantity = 0;
+}
+
+Products::Product::Product(string name, float price, string manufacture, int age, int quantity)
+{
+    this->name = name;
+    this->price = price;
+    this->manufacture = manufacture;
+    this->age = age;
+    this->quantity = quantity;
+}
+
+Products::Product::~Product()
+{
+}
+
+string Products::Product::getName() const
+{
+    return name;
+}
+
+float Products::Product::getPrice() const
+{
+    return price;
+}
+
+string Products::Product::getManufacture() const
+{
+    return manufacture;
+}
+
+int Products::Product::getQuantity() const
+{
+    return quantity;
+}
+
+int Products::Product::getAge() const
+{
+    return age;
+}
+
+
+void Products::Product::setName(string Name)
+{
+    this->name = name;
+}
+
+void Products::Product::setPrice(float Price)
+{
+    this->price = price;
+}
+
+void Products::Product::setManufacture(string Manufacture)
+{
+    this->manufacture = manufacture;
+}
+
+void Products::Product::setQuantity(int quantity)
+{
+    this->quantity = quantity;
+}
+
+void Products::Product::setAge(int Age)
+{
+    this->age = age;
+}
